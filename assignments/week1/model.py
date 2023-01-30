efe20a2e89fc3fe4690c35bd4b3ebffe196e8b36
Tamjid Imtiaz <tamjidimtiaz1995@gmail.com>
2023-01-30 10:21:43 -0500

    Homework Submission

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -5,13 +5,18 @@ class LinearRegression:
     """
     A linear regression model to fit the training data.
     """
-    w: np.ndarray
-    b: float
+
+    # w: np.ndarray
+    # b: float
 
     def __init__(self):
         self.w = []
         self.b = []
         return None
+
+        # self.w = np.randn(self.X.shape[0])
+        # self.b = np.random.random_sample()
+        # print(self.X.shape)
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         """
@@ -20,20 +25,20 @@ class LinearRegression:
             X (np.ndarray): The input data.
             y (np.ndarray): The output.
         Returns:
-            np.ndarray: weight and bias terms.
+            np.ndarray: The predicted output.
 
-        """      
-        # define a new matrix comprising of a column of ones and append it with the original 'X'
-        X_new = np.append(np.ones((X.shape[0],1)), X, 1)
-        # Calculate the weights
+        """
+        X_new = np.append(np.ones((X.shape[0], 1)), X, 1)
+        y = y
         weights = np.linalg.pinv(X_new.T @ X_new) @ (X_new.T @ y)
-
-        # calculate the bias and weights from the vector 'weights'
+        print(weights.shape)
         self.b = weights[0]
         self.w = weights[1:]
+        print(self.b)
+        print(self.w)
         return self.w, self.b
-    
-    def predict(self, X: np.ndarray) -> np.ndarray:   
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
         """
         Predict the output for the given input.
 
@@ -60,25 +65,23 @@ class GradientDescentLinearRegression(LinearRegression):
         Arguments:
             X (np.ndarray): The input data.
             y (np.ndarray): The output.
-            lr (float): learning rate
-            epochs (int): number of epochs.
+            lr (float): learning rate.
+            epochs (int): Number of Epochs.
         Returns:
-            np.ndarray: weight and bias terms.
+            np.ndarray: The predicted output.
 
-        """   
-        # define a new matrix comprising of a column of ones and append it with the original 'X'
-        X = np.append(np.ones((X.shape[0],1)), X, 1)
-        # initialize the weight matrix
+        """
+        X = np.append(np.ones((X.shape[0], 1)), X, 1)
         w = np.zeros(X.shape[1])
         for i in range(epochs):
             y_hat = X @ w.T
-            df_dm =  (-2/X.shape[0]) * (X.T @ (y - y_hat))
+            df_dm = (-2 / X.shape[0]) * (X.T @ (y - y_hat))
+            # df_dm = df_dm.reshape(len(df_dm),-1)
             w = w - df_dm * lr
             self.b = np.array(w[0])
             self.w = np.array(w[1:])
-            
+
         return self.w, self.b
-        
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
