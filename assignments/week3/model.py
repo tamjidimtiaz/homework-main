18ded7cf9434522d17065f6ebf0cca4418d7ba5e
Tamjid Imtiaz <tamjidimtiaz1995@gmail.com>
2023-02-03 16:30:57 -0500

    Submission

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,8 +1,12 @@
-import torch
 from typing import Callable
+import torch
 
 
-class MLP:
+class MLP(torch.nn.Module):
+    """
+    A multi-layer perceptron network to fit the training data.
+    """
+
     def __init__(
         self,
         input_size: int,
@@ -10,7 +14,7 @@ class MLP:
         num_classes: int,
         hidden_count: int = 1,
         activation: Callable = torch.nn.ReLU,
-        initializer: Callable = torch.nn.init.ones_,
+        initializer: Callable = torch.nn.init.kaiming_normal_,
     ) -> None:
         """
         Initialize the MLP.
@@ -22,9 +26,18 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
 
-    def forward(self, x):
+        super().__init__()
+        self.layers = torch.nn.ModuleList()
+        self.activation = activation()
+
+        self.fc1 = torch.nn.Linear(input_size, hidden_size)
+        initializer(self.fc1.weight)
+        self.fc2 = torch.nn.Linear(hidden_size, num_classes)
+        initializer(self.fc2.weight)
+
+
+    def forward(self, x: torch.Tensor) -> None:
         """
         Forward pass of the network.
 
@@ -34,4 +47,12 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        # Flatten inputs to 2D (if more than that)
+        x = x.view(x.shape[0], -1)
+
+        # Get activations of each layer
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        x = torch.nn.functional.log_softmax(x, dim=1)
+
+        return self.x
--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -15,4 +15,6 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         MLP: The created model.
 
     """
-    return MLP(input_dim, 32, output_dim, 1, torch.nn.ReLU, torch.nn.init.ones_)
+    return MLP(
+        input_dim, 256, output_dim, 1, torch.nn.ReLU, torch.nn.init.kaiming_normal_
+    )
