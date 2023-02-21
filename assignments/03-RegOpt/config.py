4622ad1efa769ea4cb3d2402c9b23eb3f9b0cc98
Tamjid Imtiaz <tamjidimtiaz1995@gmail.com>
2023-02-21 00:52:00 -0500

    Submission

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -2,29 +2,35 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, Normalize, ToTensor
+from torchvision.transforms import (
+    Compose,
+    Normalize,
+    ToTensor,
+    RandomHorizontalFlip,
+    RandomCrop,
+)
 
 
 class CONFIG:
 
     batch_size = 64
-    num_epochs = 2
-    initial_learning_rate = 0.001
+    num_epochs = 11
+    initial_learning_rate = 0.01
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "T_max": 1280,
+        "eta_min": 0.00001,
+        "last_epoch": -1,
     }
-
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(), lr=0.01, betas=(0.9, 0.999), eps=0.1, weight_decay=1e-04
     )
 
     transforms = Compose(
-        [
-            ToTensor(),
-        ]
+        [ToTensor(), Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]
     )
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,17 @@
 from typing import List
-
+import math
 from torch.optim.lr_scheduler import _LRScheduler
+import warnings
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    Here is my custom learning rate scheduler
+    """
+
+    def __init__(
+        self, optimizer, T_max=1280, eta_min=0.00001, last_epoch=-1, verbose=False
+    ):
         """
         Create a new scheduler.
 
@@ -13,12 +20,38 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
-        super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        self.T_max = T_max
+        self.eta_min = eta_min
+        self.optimizer = optimizer
+        super(CustomLRScheduler, self).__init__(self.optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
-
+        """
+        Get new learning rate from the scheduler.
+        """
         # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        if self.last_epoch == 0:
+            return [group["lr"] for group in self.optimizer.param_groups]
+        elif self._step_count == 1 and self.last_epoch > 0:
+            return [
+                self.eta_min
+                + (base_lr - self.eta_min)
+                * (1 + math.cos((self.last_epoch) * math.pi / self.T_max))
+                / 2
+                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
+            ]
+        elif (self.last_epoch - 1 - self.T_max) % (2 * self.T_max) == 0:
+            return [
+                group["lr"]
+                + (base_lr - self.eta_min) * (1 - math.cos(math.pi / self.T_max)) / 2
+                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
+            ]
+        return [
+            (1 + math.cos(math.pi * self.last_epoch / self.T_max))
+            / (1 + math.cos(math.pi * (self.last_epoch - 1) / self.T_max))
+            * (group["lr"] - self.eta_min)
+            + self.eta_min
+            for group in self.optimizer.param_groups
+        ]
