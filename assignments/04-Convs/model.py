226c32f98b2a10bb4308d441d77cc3869358ea7e
Tamjid Imtiaz <tamjidimtiaz1995@gmail.com>
2023-03-18 01:34:36 -0400

    Submission

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -10,35 +10,18 @@ class Model(torch.nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
-
-        self.convolutional_layer = nn.Sequential(
-            nn.Conv2d(
-                in_channels=num_channels, out_channels=6, kernel_size=5, stride=1
-            ),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-            nn.BatchNorm2d(6),
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
-            nn.ReLU(),
-        )
-
-        self.linear_layer = nn.Sequential(
-            nn.Linear(in_features=120, out_features=84),
-            nn.ReLU(),
-            nn.BatchNorm1d(84),
-            nn.Linear(in_features=84, out_features=num_classes),
-        )
+        self.conv1 = nn.Conv2d(num_channels, 16, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.fc = nn.Linear(32 * 8 * 8, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Forward pass of ConvNet
         """
 
-        x = self.convolutional_layer(x)
-        x = torch.flatten(x, 1)
-        x = self.linear_layer(x)
-        x = F.softmax(x, dim=1)
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 8 * 8)
+        x = self.fc(x)
         return x
