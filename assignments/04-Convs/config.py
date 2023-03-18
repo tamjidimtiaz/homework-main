f0fe7aca6cd9a53ce477971ce04c5c9442535de8
Tamjid Imtiaz <tamjidimtiaz1995@gmail.com>
2023-03-18 00:36:43 -0400

    Submission
    Submission

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,15 +2,32 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, ToTensor
+from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
-    batch_size = 8
-    num_epochs = 8
+    # batch_size = 128
+    # num_epochs = 8
+
+    # optimizer_factory: Callable[
+    #     [nn.Module], torch.optim.Optimizer
+    # ] = lambda model: torch.optim.Adam(model.parameters(), lr=5e-3)
+
+    # transforms = Compose([ToTensor()])
+    batch_size = 64
+    num_epochs = 11
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
+    ] = lambda model: torch.optim.Adam(
+        model.parameters(),
+        lr=0.01,
+        betas=(0.9, 0.999),
+        eps=0.1,
+        weight_decay=1e-4,
+    )
 
-    transforms = Compose([ToTensor()])
+    transforms = Compose(
+        [ToTensor(), Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]
+    )
+
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,44 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
+    """
+    Convolutional Neural Network
+    """
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        super(Model, self).__init__()
+
+        self.convolutional_layer = nn.Sequential(
+            nn.Conv2d(
+                in_channels=num_channels, out_channels=6, kernel_size=5, stride=1
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.BatchNorm2d(6),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
+            nn.ReLU(),
+        )
+
+        self.linear_layer = nn.Sequential(
+            nn.Linear(in_features=120, out_features=84),
+            nn.ReLU(),
+            nn.BatchNorm1d(84),
+            nn.Linear(in_features=84, out_features=num_classes),
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        """
+        Forward pass of ConvNet
+        """
+
+        x = self.convolutional_layer(x)
+        x = torch.flatten(x, 1)
+        x = self.linear_layer(x)
+        x = F.softmax(x, dim=1)
+        return x
